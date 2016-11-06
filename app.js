c696dfef98e9d0051038e85443b4bcbbfafccfc2
Giedrius Kudelis <giedrius.kudelis@gmail.com>
2016-11-06 08:00:50 +0000

    Add ability to reset play using button

--- app.js
+++ app.js
@@ -1,84 +1,99 @@
 $(function() {
     var context = new (window.AudioContext || window.webkitAudioContext);
 
-    var githubFetch = $.get('https://api.github.com/repos/airbnb/knowledge-repo/commits');
     var bufferLoad = loadBuffers();
+    var allSources = [];
 
-    $.when(githubFetch, bufferLoad)
-        .then(function(repository, buffers) {
-            var drumshot = buffers[0];
-            var crashcymbal = buffers[1];
+    $("#load-new").click(function() {
+        allSources.map(function(source) { source.disconnect() });
+        allSources = [];
+        playRepo('https://api.github.com/repos/airbnb/knowledge-repo/commits');
+    });
 
-            var commitTiming = repository[0].reverse().map(function(commitData) {
-                return Date.parse(commitData.commit.author.date) / 1000;
-            }).sort();
-            var repoStart = commitTiming.reduce(function(acc, time) {
-                return time < acc ? time : acc;
-            }, Infinity);
-            var repoEnd = commitTiming.reduce(function(acc, time) {
-                return time > acc ? time : acc;
-            }, 0);
-            var repoDuration = repoEnd - repoStart;
-            var relativeTiming = commitTiming.map(function(time) {
-                return time - repoStart;
+    //var githubFetch = $.get('https://api.github.com/repos/airbnb/knowledge-repo/commits');
+
+    function playRepo(repo) {
+        $.when($.get(repo), bufferLoad)
+            .then(function(repository, buffers) {
+                var drumshot = buffers[0];
+                var crashcymbal = buffers[1];
+                var tomtom = buffers[2];
+
+                var commitTiming = repository[0].reverse().map(function(commitData) {
+                    return Date.parse(commitData.commit.author.date) / 1000;
+                }).sort();
+                var repoStart = commitTiming.reduce(function(acc, time) {
+                    return time < acc ? time : acc;
+                }, Infinity);
+                var repoEnd = commitTiming.reduce(function(acc, time) {
+                    return time > acc ? time : acc;
+                }, 0);
+                var repoDuration = repoEnd - repoStart;
+                var relativeTiming = commitTiming.map(function(time) {
+                    return time - repoStart;
+                });
+
+                console.log(repoStart);
+                console.log(repoEnd);
+                console.log(repoDuration);
+                console.log(relativeTiming);
+
+                // measure the shortest space between commits
+                var shortestPeriod = Infinity;
+                for (var i = 0; i < relativeTiming.length-1; i++) {
+                    var period = relativeTiming[i+1] - relativeTiming[i];
+                    shortestPeriod = period < shortestPeriod ? period : shortestPeriod;
+                }
+
+                // drop BPM by no more than 10x
+                var scaling = timeScalingFactory(0.5, 10, 0.00001, shortestPeriod);
+
+                // create drum timings with all the scaling done
+                var beatStart = context.currentTime;
+                var drumTimings = [];
+                for (var i = 0; i < relativeTiming.length-1; i++) {
+                    var period = scaling(relativeTiming[i+1] - relativeTiming[i]);
+
+                    var periodU = relativeTiming[i+1] - relativeTiming[i];
+                    var lastPeriodU = relativeTiming[i] - relativeTiming[i-1];
+                    var lastLastPeriodU = relativeTiming[i-1] - relativeTiming[i-2];
+                    var firstBuffer = drumshot;
+                    if (i > 1) {
+                        if (lastLastPeriodU > lastPeriodU && lastPeriodU < periodU) {
+                            firstBuffer = crashcymbal;
+                        } else if (lastLastPeriodU < lastPeriodU && lastPeriodU > periodU) {
+                            firstBuffer = tomtom;
+                        }
+                    }
+
+                    drumTimings.push({
+                            buffer: firstBuffer,
+                            time: beatStart + 0 * period
+                        });
+                    drumTimings.push({
+                            buffer: drumshot,
+                            time: beatStart + 0.25 * period
+                        });
+                    drumTimings.push({
+                            buffer: drumshot,
+                            time: beatStart + 0.5 * period
+                        });
+                    drumTimings.push({
+                            buffer: drumshot,
+                            time: beatStart + 0.75 * period
+                        });
+
+                    beatStart += period;
+                }
+
+                console.log(drumTimings);
+                drumTimings.map(function(t) {
+                    playSound(t.buffer, t.time)
+                });
+            }, function(error) {
+                console.log(error);
             });
-
-            console.log(repoStart);
-            console.log(repoEnd);
-            console.log(repoDuration);
-            console.log(relativeTiming);
-
-            // 4/4 beat
-            var timingsInBeat = [0, 0.25, 0.5, 0.75];
-            // measure the shortest space between commits
-            var shortestPeriod = Infinity;
-            for (var i = 0; i < relativeTiming.length-1; i++) {
-                var period = relativeTiming[i+1] - relativeTiming[i];
-                shortestPeriod = period < shortestPeriod ? period : shortestPeriod;
-            }
-
-            // drop BPM by no more than 10x
-            var scaling = timeScalingFactory(0.5, 10, 0.00001, shortestPeriod);
-
-            // create drum timings with all the scaling done
-            var beatStart = 0;
-            var drumTimings = [];
-            for (var i = 0; i < relativeTiming.length-1; i++) {
-                var period = scaling(relativeTiming[i+1] - relativeTiming[i]);
-
-                var periodU = relativeTiming[i+1] - relativeTiming[i];
-                var lastPeriodU = relativeTiming[i] - relativeTiming[i-1];
-                var lastLastPeriodU = relativeTiming[i-1] - relativeTiming[i-2];
-                var firstBuffer = drumshot;
-                if (i > 1 && lastLastPeriodU > lastPeriodU && lastPeriodU < periodU) { firstBuffer = crashcymbal }
-
-                drumTimings.push({
-                        buffer: firstBuffer,
-                        time: beatStart + 0 * period
-                    });
-                drumTimings.push({
-                        buffer: drumshot,
-                        time: beatStart + 0.25 * period
-                    });
-                drumTimings.push({
-                        buffer: drumshot,
-                        time: beatStart + 0.5 * period
-                    });
-                drumTimings.push({
-                        buffer: drumshot,
-                        time: beatStart + 0.75 * period
-                    });
-
-                beatStart += period;
-            }
-
-            console.log(drumTimings);
-            drumTimings.map(function(t) {
-                playSound(t.buffer, t.time)
-            });
-        }, function(error) {
-            console.log(error);
-        });
+    }
 
     function timeScalingFactory(A, v, k, tmin) {
         return function(t) { return v*A - (v-1)*A* Math.exp(-k*(t-tmin)) }
@@ -88,7 +103,8 @@ $(function() {
         var bufferPromise = $.Deferred();
         var bufferLoader = new BufferLoader(context, [
                 'samples/drumshot.wav',
-                'samples/crashcymbal.wav'
+                'samples/crashcymbal.wav',
+                'samples/tomtom.wav'
                 ], function(buffers) { bufferPromise.resolve(buffers) });
         bufferLoader.load();
         return bufferPromise;
@@ -99,5 +115,6 @@ $(function() {
         source.buffer = buffer;
         source.connect(context.destination);
         source.start(time);
+        allSources.push(source);
     }
 });
